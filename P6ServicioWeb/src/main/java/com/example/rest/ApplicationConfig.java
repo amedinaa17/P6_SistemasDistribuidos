db0ce1abf46eb63bdf2d2166057302989fb5e7e6
Ana Medina <133587036+amedinaa17@users.noreply.github.com>
2025-04-02 14:24:46 -0600

    Update ApplicationConfig.java

--- P6ServicioWeb/src/main/java/com/example/rest/ApplicationConfig.java
+++ P6ServicioWeb/src/main/java/com/example/rest/ApplicationConfig.java
@@ -5,12 +5,11 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
- * Configuración de la aplicación REST.
- * Esta clase configura la ruta base para los servicios web RESTful en la aplicación.
- * Define el prefijo para todas las rutas de la API.
- * 
+ * Configuración de la aplicación REST. Esta clase configura la ruta base para
+ * los servicios web RESTful en la aplicación.
+ *
  * @author aname
  */
-@ApplicationPath("/api")  // Define la ruta base de la API
+@ApplicationPath("/restful")  // Define la ruta base
 public class ApplicationConfig extends Application {
 }
